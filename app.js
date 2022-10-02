060dd88f23cc6ebfe03c4ca2edced8177c685373
Kemal Emre Erginkoç <keerginkoc@gmail.com>
2022-10-02 22:34:15 +0300

    Real-Time Character Counter Finished

---
+++ app.js
@@ -0,0 +1,12 @@
+const textareaEl = document.getElementById("textarea");
+const totalCounterEl = document.getElementById("total-counter");
+const remainingEl = document.getElementById("remaining")
+
+textareaEl.addEventListener("keyup", ()=>{
+    updateCounter()
+})
+
+function updateCounter(){
+    totalCounterEl.innerText = textareaEl.value.length;
+    remainingEl.innerText = textareaEl.getAttribute("maxLength") - textareaEl.value.length;
+}
